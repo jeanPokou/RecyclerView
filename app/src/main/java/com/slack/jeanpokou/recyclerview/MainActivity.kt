6e57adc947476b1b768bbef5fac8848a7706b976
jeanpokou <pokoujeanmarc@gmail.com>
2017-09-19 23:32:31 -0300

    Wiring Recyler View

--- app/src/main/java/com/slack/jeanpokou/recyclerview/MainActivity.kt
+++ app/src/main/java/com/slack/jeanpokou/recyclerview/MainActivity.kt
@@ -2,11 +2,25 @@ package com.slack.jeanpokou.recyclerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.LayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private  val NUM_LIST_ITEMS = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(rv_numbers) {
+            adapter = GreenAdapter(NUM_LIST_ITEMS)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            hasFixedSize()
+        }
+
+
     }
 }
